58d4b2e0ac0f7fcadeeca2b9aa05429eea1a4ef9
oato.o <oat9002@gmail.com>
2016-10-08 17:18:23 +0700

    Edited mockup

--- service/TwitterService.js
+++ service/TwitterService.js
@@ -1,7 +1,7 @@
 export function mockupOutput() {
   let output = {}
   let max = 100
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 5; i++) {
     switch (i) {
       case 0:
         output.happy = Math.floor(Math.random() * (max + 1))
@@ -12,10 +12,14 @@ export function mockupOutput() {
         max = max - output.sad
         break;
       case 2:
+        output.fear = Math.floor(Math.random() * (max + 1))
+        max = max - output.fear
+        break;
+      case 3:
         output.angry = Math.floor(Math.random() * (max + 1))
         max = max - output.angry
         break;
-      case 3:
+      case 4:
         output.neutral = max
         break;
       default: console.log("something went wrong.");
