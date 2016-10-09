c8ad367958c2d8bba02df4e4d87c20c9f5421634
oato.o <oat9002@gmail.com>
2016-10-09 15:40:17 +0700

    Fixed using nested loop

--- service/TwitterService.js
+++ service/TwitterService.js
@@ -1,30 +1,29 @@
 export function mockupOutput() {
+  let arrayOutput = new Array(5)
+  for (let i = 0; i < 5; i++) {
+    randomSentiments(i).then(output => {
+      arrayOutput[i] = output
+    })
+  }
+  return new Promise((resolve, reject) => {
+    resolve(arrayOutput)
+  })
+}
+
+function randomSentiments(value) {
   let output = {}
   let max = 100
-  for (let i = 0; i < 5; i++) {
-    switch (i) {
-      case 0:
-        output.happy = Math.floor(Math.random() * (max + 1))
-        max = max - output.happy
-        break;
-      case 1:
-        output.sad = Math.floor(Math.random() * (max + 1))
-        max = max - output.sad
-        break;
-      case 2:
-        output.fear = Math.floor(Math.random() * (max + 1))
-        max = max - output.fear
-        break;
-      case 3:
-        output.angry = Math.floor(Math.random() * (max + 1))
-        max = max - output.angry
-        break;
-      case 4:
-        output.neutral = max
-        break;
-      default: console.log("something went wrong.");
-    }
-  }
+  output.happy = Math.floor(Math.random() * (max + 1))
+  max = max - output.happy
+  output.sad = Math.floor(Math.random() * (max + 1))
+  max = max - output.sad
+  output.fear = Math.floor(Math.random() * (max + 1))
+  max = max - output.fear
+  output.angry = Math.floor(Math.random() * (max + 1))
+  max = max - output.angry
+  output.neutral = max
+  output.latitude = Math.random() * 15
+  output.logitude = Math.random() * 130
   return new Promise((resolve, reject) => {
     resolve(output)
   })
